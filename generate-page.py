9a7153f0c7c588b917290fdefac166af082cfc4e
Luke Horwell <lukehorwell37+code@gmail.com>
2016-07-12 19:52:24 +0100

    Add further optimisations and generate into a single file.

--- generate-page.py
+++ generate-page.py
@@ -5,12 +5,18 @@ import os
 import inspect
 import glob
 import shutil
+import base64
 
 ######################################
 try:
     import csscompressor
 except:
     print("Requires 'python3-csscompressor' package.")
+    exit(1)
+
+if not os.path.exists("/usr/bin/convert"):
+    print("Requires 'convert' command from imagemagick package.")
+    exit(1)
 
 ######################################
 def load(path):
@@ -51,6 +57,16 @@ def replace(filename, old, new):
     newdata = data.replace(old, new)
     save(path, newdata)
 
+def convert_base64(filename):
+    path = os.path.join(build_dir, filename)
+    with open(path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+    return(str(encoded_string))
+
+def convert_img(filename):
+    newname = filename.split('.')[0] + '.jpg'
+    os.system("convert -flatten {0} {1}".format(filename, newname))
+
 ######################################
 cur_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 build_dir = os.path.join(cur_dir, "build")
@@ -69,6 +85,7 @@ for ext in ["ico", "png", "html", "css"]:
         shutil.copy(file, build_dir)
 
 # Perform optimisation
+# --> Compress Pages
 compress("index.html")
 strip("start.css")
 replace("index.html", \
@@ -76,9 +93,18 @@ replace("index.html", \
         "<style>" + load("start.css") + "</style>")
 replace("index.html", "><", ">\n<")
 
-# Clean up unused files
-for file in ["start.css"]:
-    os.remove(os.path.join(build_dir, file))
+# --> Compress & Embed Images
+replace("index.html", 'favicon.ico', 'data:image/ico;base64,' + convert_base64("favicon.ico"))
 
-# Done!
+convert_img("logo.png")
+replace("index.html", 'logo.png', 'data:image/jpg;base64,' + convert_base64("logo.jpg"))
+
+convert_img("spritesheet.png")
+replace("index.html", 'spritesheet.png', 'data:image/jpg;base64,' + convert_base64("spritesheet.jpg"))
+
+# --> Clean up unused files
+for ext in ["css", "ico", "png", "jpg"]:
+    path = glob.glob(build_dir + '/*.' + ext)
+    for file in path:
+        os.remove(file)
 print("Finished!")
