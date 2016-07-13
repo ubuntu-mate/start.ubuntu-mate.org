26faccd7944259a65470047b2f1ae362e0f728c5
Luke Horwell <lukehorwell37+code@gmail.com>
2016-07-13 20:07:13 +0100

    Add radio buttons to switch between Google and DuckDuckGo.
    Google is the default.

--- start.js
+++ start.js
@@ -1,1 +1,37 @@
+/* Code for storing/processing settings for this page */
 
+/* Defaults */
+if ( localStorage.getItem("search") == null ) {
+    localStorage.setItem("search", "ggl");
+}
+
+function hide(div) {
+    document.getElementsByClassName(div)[0].style.display = 'none';
+}
+
+function show(div) {
+    document.getElementsByClassName(div)[0].style.display = 'initial';
+}
+
+/* Change preferred search engine */
+function setSearch(engine) {
+    localStorage.setItem("search", engine);
+    getSearch();
+}
+
+/* Show/hide search engines when requested */
+function getSearch() {
+    engine = localStorage.getItem("search");
+    hide("ggl");
+    hide("ddg");
+    if ( engine == "ddg" ) {
+        show("ddg");
+        document.getElementById("ddg").checked = true;
+    } else {
+        show("ggl");
+        document.getElementById("ggl").checked = true;
+    }
+}
+
+/* On page entrance */
+getSearch();
