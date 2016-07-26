23f06f3457b6177bc3664bb03efd66f08939f412
Martin Wimpress <code@flexion.org>
2016-07-26 16:51:58 +0100

    Switch deployment to new servers.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,9 @@
 sudo apt-get -y install rsync
 
 if [ -f build/index.html ]; then
-    for HOST in can fra; do
+    for HOST in nyc sf; do
         echo "Deploying to: ${HOST}"
-        rsync -av -e "ssh -o StrictHostKeyChecking=no" --progress --delete build/ matey@${HOST}.wimpress.io:start.ubuntu-mate.org/
+        rsync -av -e "ssh -o StrictHostKeyChecking=no" --progress --delete build/ matey@${HOST}.ubuntu-mate.net:start.ubuntu-mate.org/
     done
 else
     echo "ERROR! build/index.html was not found."
