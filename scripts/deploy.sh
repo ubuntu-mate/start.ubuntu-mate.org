439c6c1e913ca8d26af006dfcb3fca6f16be84da
Martin Wimpress <code@flexion.org>
2016-07-28 13:19:24 +0100

    Update deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y install rsync
 
 if [ -f build/index.html ]; then
-    for HOST in nyc sf; do
+    for HOST in nyc sf ger; do
         echo "Deploying to: ${HOST}"
         rsync -av -e "ssh -o StrictHostKeyChecking=no" --progress --delete build/ matey@${HOST}.ubuntu-mate.net:start.ubuntu-mate.org/
     done
