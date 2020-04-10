41051a5733c5ef4eda2277d7763cd17489b27fc7
Luke Horwell <code@horwell.me>
2020-04-10 10:14:08 +0100

    i18n: Ensure correct working directory

--- scripts/sync-translations.sh
+++ scripts/sync-translations.sh
@@ -2,6 +2,8 @@
 #
 # Can only be run by the Ubuntu MATE team who have write access to Transifex.
 #
+cd "$(dirname "$0")/../_includes/i18n/"
+
 tx pull -a --minimum-perc=100
 json2po -i default.json -o locales.pot
 tx push -s
